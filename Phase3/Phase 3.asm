610c7719715282c505cda54e497db8f8c1c170c0
Chrisbarbosa711 <chrisbarbosa711@gmail.com>
2024-11-24 17:49:52 -0500

    organizational changes for readability

--- Phase3/Phase 3.asm
+++ Phase3/Phase 3.asm
@@ -18,191 +18,199 @@ syscall #Accepts and stores the user's input into v0.
 
 move $t6, $v0
 
-j menuSelect #Jumps to the menu select subroutine.
+j menuSelect #Jumps to the menu select subroutine. (NOTE)not really necessary since the flow of code will always go to menu select anyway
 
 menuSelect:
-la $a0, menu #Loads the menu into the a0 register.
-li $v0, 4 #Prepares to print a string.
-syscall #Prints the string that is held in a0, in this case menu.
+	la $a0, menu 		#Loads the menu into the a0 register.
+	li $v0, 4 		#Prepares to print a string.
+	syscall 		#Prints the string that is held in a0, in this case menu.
 
-li $v0, 5 #Prepares to accept a user's inputted menu choice.
-syscall #Accepts and stores the user's input into v0.
+	li $v0, 5 		#Prepares to accept a user's inputted menu choice.
+	syscall 		#Accepts and stores the user's input into v0.
 
-la $t0, ($v0) #Stores the selected menu option into t0.
+	la $t0, ($v0) 		#Stores the selected menu option into t0.
 
-beq $t0, -1, exit #If the user input is -1, which is the exit code, jumps to the exit subroutine.
-beq $t0, 1, water #If the user input is 1, which is the water option, jumps to the water subroutine.
-beq $t0, 2, snacks #If the user input is 2, which is the snacks option, jumps to the snacks subroutine.
-beq $t0, 3, sandwhiches #If the user input is 3, which is the sandwhiches, jumps to the sandwhiches subroutine.
-beq $t0, 4, meals #If the user input is 4, which is the meals option, jumps to the meals subroutine.
+# the following parses the input and jumps to the different options based on the user input, represented here as beq statements
+	beq $t0, -1, exit 	#If the user input is -1, which is the exit code, jumps to the exit subroutine.
+	beq $t0, 1, water 	#If the user input is 1, which is the water option, jumps to the water subroutine.
+	beq $t0, 2, snacks 	#If the user input is 2, which is the snacks option, jumps to the snacks subroutine.
+	beq $t0, 3, sandwhiches #If the user input is 3, which is the sandwhiches, jumps to the sandwhiches subroutine.
+	beq $t0, 4, meals 	#If the user input is 4, which is the meals option, jumps to the meals subroutine.
 
-water:
-la $a0, quantity
-li $v0, 4
-syscall
+#
+	water:
+		la $a0, quantity
+		li $v0, 4
+		syscall
 
-li $v0, 5
-syscall
+		li $v0, 5
+		syscall
 
-add $t2, $zero, $zero
-add $t3, $zero, $v0
-addi $t2, $t2, 2
-mul $t1, $t2, $t3
+		add $t2, $zero, $zero
+		add $t3, $zero, $v0
+		addi $t2, $t2, 2
+		mul $t1, $t2, $t3
 
-slt $at, $t6, $t1
-bne $at, $zero, error
+		slt $at, $t6, $t1
+		bne $at, $zero, error
 
-sub $t6, $t6, $t1
+		sub $t6, $t6, $t1
 
-la $a0, purchaseComplete
-li $v0, 4
-syscall
+		la $a0, purchaseComplete
+		li $v0, 4
+		syscall
 
-la $a0, amtPurchased
-li $v0, 4
-syscall
+		la $a0, amtPurchased
+		li $v0, 4
+		syscall
 
-la $a0, ($t3)
-li $v0, 1
-syscall
+		la $a0, ($t3)
+		li $v0, 1
+		syscall
 
-la $a0, remainBal
-li $v0, 4
-syscall
+		la $a0, remainBal
+		li $v0, 4
+		syscall
 
-la $a0, ($t6)
-li $v0, 1
-syscall
+		la $a0, ($t6)
+		li $v0, 1
+		syscall
 
-j menuSelect
-snacks:
-la $a0, quantity
-li $v0, 4
-syscall
+		j menuSelect		#return back to select screen for next input
 
-li $v0, 5
-syscall
+	snacks:
+		la $a0, quantity
+		li $v0, 4
+		syscall
 
-add $t2, $zero, $zero
-add $t3, $zero, $v0
-addi $t2, $t2, 1
-mul $t1, $t2, $t3
+		li $v0, 5
+		syscall
 
-slt $at, $t6, $t1
-bne $at, $zero, error
+		add $t2, $zero, $zero
+		add $t3, $zero, $v0
+		addi $t2, $t2, 1
+		mul $t1, $t2, $t3
 
-sub $t6, $t6, $t1
+		slt $at, $t6, $t1
+		bne $at, $zero, error
 
-la $a0, purchaseComplete
-li $v0, 4
-syscall
+		sub $t6, $t6, $t1
 
-la $a0, amtPurchased
-li $v0, 4
-syscall
+		la $a0, purchaseComplete
+		li $v0, 4
+		syscall
 
-la $a0, ($t3)
-li $v0, 1
-syscall
+		la $a0, amtPurchased
+		li $v0, 4
+		syscall
 
-la $a0, remainBal
-li $v0, 4
-syscall
+		la $a0, ($t3)
+		li $v0, 1
+		syscall
 
-la $a0, ($t6)
-li $v0, 1
-syscall
+		la $a0, remainBal
+		li $v0, 4
+		syscall
 
-j menuSelect
-sandwhiches:
-la $a0, quantity
-li $v0, 4
-syscall
+		la $a0, ($t6)
+		li $v0, 1
+		syscall
 
-li $v0, 5
-syscall
+		j menuSelect 		#return for next input
 
-add $t2, $zero, $zero
-add $t3, $zero, $v0
-addi $t2, $t2, 5
-mul $t1, $t2, $t3
+	sandwhiches:
+		la $a0, quantity
+		li $v0, 4
+		syscall
 
-slt $at, $t6, $t1
-bne $at, $zero, error
+		li $v0, 5
+		syscall
 
-sub $t6, $t6, $t1
+		add $t2, $zero, $zero
+		add $t3, $zero, $v0
+		addi $t2, $t2, 5
+		mul $t1, $t2, $t3
 
-la $a0, purchaseComplete
-li $v0, 4
-syscall
+		slt $at, $t6, $t1
+		bne $at, $zero, error
 
-la $a0, amtPurchased
-li $v0, 4
-syscall
+		sub $t6, $t6, $t1
 
-la $a0, ($t3)
-li $v0, 1
-syscall
+		la $a0, purchaseComplete
+		li $v0, 4
+		syscall
 
-la $a0, remainBal
-li $v0, 4
-syscall
+		la $a0, amtPurchased
+		li $v0, 4
+		syscall
 
-la $a0, ($t6)
-li $v0, 1
-syscall
+		la $a0, ($t3)
+		li $v0, 1
+		syscall
 
-j menuSelect
-meals:
-la $a0, quantity
-li $v0, 4
-syscall
+		la $a0, remainBal
+		li $v0, 4
+		syscall
 
-li $v0, 5
-syscall
+		la $a0, ($t6)
+		li $v0, 1
+		syscall
 
-add $t2, $zero, $zero
-add $t3, $zero, $v0
-addi $t2, $t2, 10
-mul $t1, $t2, $t3
+		j menuSelect		#return to for next input
 
-slt $at, $t6, $t1
-bne $at, $zero, error
+	meals:
+		la $a0, quantity
+		li $v0, 4
+		syscall
 
-sub $t6, $t6, $t1
+		li $v0, 5
+		syscall
 
-la $a0, purchaseComplete
-li $v0, 4
-syscall
+		add $t2, $zero, $zero
+		add $t3, $zero, $v0
+		addi $t2, $t2, 10
+		mul $t1, $t2, $t3
 
-la $a0, amtPurchased
-li $v0, 4
-syscall
+		slt $at, $t6, $t1
+		bne $at, $zero, error
 
-la $a0, ($t3)
-li $v0, 1
-syscall
+		sub $t6, $t6, $t1
 
-la $a0, remainBal
-li $v0, 4
-syscall
+		la $a0, purchaseComplete
+		li $v0, 4
+		syscall
 
-la $a0, ($t6)
-li $v0, 1
-syscall
+		la $a0, amtPurchased
+		li $v0, 4
+		syscall
 
-j menuSelect
-error:
-la $a0, errorMessage
-li $v0, 4
-syscall
+		la $a0, ($t3)
+		li $v0, 1
+		syscall
 
-j menuSelect
+		la $a0, remainBal
+		li $v0, 4
+		syscall
+
+		la $a0, ($t6)
+		li $v0, 1
+		syscall
+
+		j menuSelect		#return for next input
+
+#runs when there is an issue with the option selected and the amount left of the users startBal
+error: 
+	la $a0, errorMessage
+	li $v0, 4
+	syscall
+
+	j menuSelect
+
 exit:
-la $a0, remainBal
-li $v0, 4
-syscall
+	la $a0, remainBal
+	li $v0, 4
+	syscall
 
-add $a0, $t6, $zero
-li $v0, 2
-syscall
+	add $a0, $t6, $zero
+	li $v0, 2
+	syscall
